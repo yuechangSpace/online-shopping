553cc2457f3dbba4ad4b5b612d633938fa81faf8
Yue <771397803@qq.com>
2020-06-26 22:26:05 -0400

    Updates

---
+++ precache-manifest.e6a8e9563a66aead7a1e6c991b8ed650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb59631825f483a6221d1fbf84b4cf3",
+    "url": "/online-shopping/index.html"
+  },
+  {
+    "revision": "2d1cc8e9324a5b80a346",
+    "url": "/online-shopping/static/css/main.21d3e496.chunk.css"
+  },
+  {
+    "revision": "98b0d5f58ba9265cceca",
+    "url": "/online-shopping/static/js/2.2b0abcaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/online-shopping/static/js/2.2b0abcaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1cc8e9324a5b80a346",
+    "url": "/online-shopping/static/js/main.3e53f8b6.chunk.js"
+  },
+  {
+    "revision": "6bad9d32477b21c1288d",
+    "url": "/online-shopping/static/js/runtime-main.e0c0aab1.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/online-shopping/static/media/crown.ed28accb.svg"
+  }
+]);
